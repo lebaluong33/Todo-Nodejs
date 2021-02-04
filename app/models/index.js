d9f8a67338059f38036264f016faa99392ebaf0d
Le Ba Luong <lebaluong33@gmail.com>
2021-02-04 15:12:37 +0700

    edit sequelize config

--- app/models/index.js
+++ app/models/index.js
@@ -1,19 +1,21 @@
 const dbConfig = require('../config/db.config.js');
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  port: dbConfig.PORT,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   port: dbConfig.PORT,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: false,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle,
+//   },
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const db = {};
 
